a1d3b2eae9f477932b54298038a0e74768b50737
Thibault G <thibault.gerardin@gmail.com>
2024-02-23 17:09:17 +0100

    début de mise en place, ajout prettier, react router, quelques dossiers

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,15 @@
+import './styles/Home.css';
+
+function Home() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Page d'accueil
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default Home;
